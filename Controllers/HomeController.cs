aff74337522c864c405c527f41294b3b139c0fd4
SavoryScrimp <keynan.bailey@gmail.com>
2020-05-21 15:31:00 -0700

    You may now click, and the number goes up.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using clicker_mvp_test.Models;
 
@@ -12,7 +13,6 @@ namespace clicker_mvp_test.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -20,7 +20,31 @@ namespace clicker_mvp_test.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            if(HttpContext.Session.GetInt32("TotalNumber") is null){
+                HttpContext.Session.SetInt32("TotalNumber", 0);
+                HttpContext.Session.SetInt32("ClickNumber", 1);
+            }
+            int? TotalNumber = HttpContext.Session.GetInt32("TotalNumber");
+            int? ClickNumber = HttpContext.Session.GetInt32("ClickNumber");
+
+            TotalNumber = (int)TotalNumber;
+            ClickNumber = (int)ClickNumber;
+
+            ViewBag.TotalNumber = TotalNumber;
+            return View("Index");
+        }
+        [HttpPost]
+        [Route("Click")]
+        public IActionResult Click()
+        {
+            int? TotalNumber = HttpContext.Session.GetInt32("TotalNumber");
+            int? ClickNumber = HttpContext.Session.GetInt32("ClickNumber");
+
+            TotalNumber += ClickNumber;
+            
+            HttpContext.Session.SetInt32("TotalNumber", (int)TotalNumber);
+
+            return RedirectToAction("Index");
         }
 
         public IActionResult Privacy()
